fa40bf8ad7a45adfc6963253c1a3b92aba57bf86
LeanderVanBaekel <leander.van.baekel@gmail.com>
2016-06-22 18:19:52 +0200

    update

--- dps/models/slides.js
+++ dps/models/slides.js
@@ -1,17 +1,18 @@
-var mongoose     = require('mongoose');
-var Schema       = mongoose.Schema;
+// Mongoose Schema for adding a slides to MongoDB
+
+var mongoose = require('mongoose');
+var Schema   = mongoose.Schema;
 
 var slides  = new Schema({
-  sid: Number,
-	title: String,
-	contentType: String,
-	content: String,
-  status: Boolean, // true = active
-  duration: Number,
-  createdOn : Date,
-  endsOn: Date,
-  createdBy : String
-
+  sid: Number, // slide id
+	title: String, // name of the slide for internal use
+	contentType: String, // type of the content of the slide
+	content: String, // Link or content of the slide
+  status: Boolean, // Not used at the moment
+  duration: Number, // length the slide is shown in seconds
+  createdOn : Date, // date slide got created
+  endsOn: Date, // Not used at the moment
+  createdBy : String // Name of the creator
 });
 
 module.exports = mongoose.model('Slides', slides);
